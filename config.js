e05c7c6975832f587a5d92319ed86c223183568a
monarchxyspider <raffaysweet@gmail.com>
2026-08-29 09:00:55 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,59 @@
+const ShadeMistConfig = {
+
+    botName: "ShadeMist",
+
+    ownerPassword: "YOUR_PASSWORD",
+
+    links: {
+        invite: "YOUR_INVITE_LINK",
+        support: "YOUR_SUPPORT_LINK"
+    },
+
+    images: {
+        docsHero: "YOUR_CUSTOM_IMAGE_URL"
+    },
+
+    emojis: {
+        commands: "YOUR_CUSTOM_EMOJI_URL",
+        support: "YOUR_CUSTOM_EMOJI_URL",
+        gettingStarted: "YOUR_CUSTOM_EMOJI_URL",
+        updates: "YOUR_CUSTOM_EMOJI_URL",
+        owner: "YOUR_CUSTOM_EMOJI_URL",
+        bot: "YOUR_CUSTOM_EMOJI_URL",
+        settings: "YOUR_CUSTOM_EMOJI_URL"
+    },
+
+    categories: [
+        {
+            id: "moderation",
+            name: "Moderation",
+            emoji: "moderation"
+        },
+        {
+            id: "fun",
+            name: "Fun",
+            emoji: "fun"
+        },
+        {
+            id: "general",
+            name: "General",
+            emoji: "general"
+        },
+        {
+            id: "utility",
+            name: "Utility",
+            emoji: "utility"
+        },
+        {
+            id: "welcome",
+            name: "Welcome",
+            emoji: "welcome"
+        },
+        {
+            id: "role",
+            name: "Role",
+            emoji: "role"
+        }
+    ]
+
+};
